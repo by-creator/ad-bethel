a4eaa2a53aae204a26a6d3bb48c7e4b54d648f08
Marc Bongoyeba (AGL) <marc.bongoyeba@dakar-terminal.com>
2026-01-27 12:29:21 +0000

    update

--- resources/views/inscription/wizard.blade.php
+++ resources/views/inscription/wizard.blade.php
@@ -1,5 +1,19 @@
-@include('partials.wizard.style')
+<!DOCTYPE html>
+<html lang="en">
 
-@include('partials.wizard.form')
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>INSTE | INSCRIPTION</title>
+</head>
 
-@include('partials.wizard.script')
+<body>
+    @include('partials.wizard.style')
+
+    @include('partials.wizard.form')
+
+    @include('partials.wizard.script')
+</body>
+
+</html>
